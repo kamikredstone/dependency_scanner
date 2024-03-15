833ba58f3a7367e3516262a0ce54821b4097be3e
Gil Shvarzman <gilshvarzman@Gils-Mac-Studio.local>
2024-03-15 21:45:06 +0200

    Refactor the producer + fix tests

--- producer/tests/test_file_submission.py
+++ producer/tests/test_file_submission.py
@@ -1,6 +1,8 @@
-from fastapi import HTTPException
+from fastapi import HTTPException, Request
+from starlette.datastructures import FormData
 from src.utils import handle_file_submission
 import pytest
+from unittest.mock import Mock, AsyncMock
 
 class MockUploadFile:
     def __init__(self, content):
@@ -10,41 +12,57 @@ class MockUploadFile:
         return self.content
     
     async def seek(self, offset):
-        return self.content[offset]
+        pass
 
+async def mock_request(data=None, file=None):
+    form_data = {}
+    if data:
+        form_data['data'] = data
+    if file:
+        form_data['file'] = file
+
+    request = Mock()
+    request.form = AsyncMock(return_value=form_data)
+    return request
+
+@pytest.mark.asyncio
 class TestHandleFileSubmission:
-    @pytest.mark.asyncio
+
     async def test_with_data_input(self):
         data = "test_data"
-        key, content = await handle_file_submission(data=data)
+        request = await mock_request(data=data)
+        key, content = await handle_file_submission(request)
 
         assert isinstance(key, str)
         assert isinstance(content, bytes)
         assert content == b"test_data"
 
-    @pytest.mark.asyncio
+    
     async def test_with_file_input(self):
         file_content = b"test_file_content"
         file = MockUploadFile(file_content)
-        key, content = await handle_file_submission(file=file)
+        request = await mock_request(file=file)
+        key, content = await handle_file_submission(request)
 
         assert isinstance(key, str)
         assert isinstance(content, bytes)
         assert content == file_content
 
-    @pytest.mark.asyncio
+    
     async def test_with_no_input(self):
+        request = await mock_request()
         with pytest.raises(HTTPException) as exc_info:
-            await handle_file_submission()
+            await handle_file_submission(request)
         assert exc_info.value.status_code == 400
         assert "No data or file provided" in str(exc_info.value.detail)
 
-    @pytest.mark.asyncio
+    
     async def test_with_both_inputs(self):
         file_content = b"test_file_content"
         file = MockUploadFile(file_content)
         data = "test_data"
+        request = await mock_request(file=file, data=data)
         with pytest.raises(HTTPException) as exc_info:
-            await handle_file_submission(file=file, data=data)
+            await handle_file_submission(request)
         assert exc_info.value.status_code == 400
         assert "Provide data OR file" in str(exc_info.value.detail)
